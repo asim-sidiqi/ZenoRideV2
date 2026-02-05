0a4db858cd7136b88b2091f4e13f667b938293f8
asim-sidiqi <29aseem.sid@gmail.com>
2026-02-05 10:23:07 +0530

    ui-tweaks

--- Frontend/src/pages/Start.jsx
+++ Frontend/src/pages/Start.jsx
@@ -4,7 +4,7 @@ import logo2 from '../assets/logo2.png'
 
 const Start = () => {
   return (
-    <div className='min-h-screen h-[100dvh] w-full overflow-hidden'>
+    <div className='h-[100dvh] w-full overflow-hidden'>
       {/* Mobile background image */}
       <div className='absolute inset-0 bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1619059558110-c45be64b73ae?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] md:hidden' />
       
@@ -15,14 +15,14 @@ const Start = () => {
       <div className='absolute inset-0 bg-black/20' />
       
       {/* Content */}
-      <div className='relative h-full flex flex-col justify-between'>
+      <div className='relative h-full flex flex-col'>
         {/* Logo */}
         <div className='pt-8 px-8'>
           <img className='w-28 drop-shadow-lg' src={logo2} alt="Zeno Logo" />
         </div>
         
         {/* Bottom card */}
-        <div className='text-center bg-white rounded-t-3xl shadow-2xl p-8 md:mx-auto md:mb-12 md:rounded-3xl md:max-w-md md:w-full'>
+        <div className='mt-auto text-center bg-white rounded-t-3xl shadow-2xl p-8 md:mx-auto md:mb-12 md:rounded-3xl md:max-w-md md:w-full'>
           <h2 className='text-3xl md:text-4xl font-bold text-gray-900 mb-2'>
             Get Started with Zeno
           </h2>
