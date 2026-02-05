8546253898dce1ac7449dc05c56076dc8db8303a
asim-sidiqi <29aseem.sid@gmail.com>
2026-02-05 12:57:47 +0530

    ui-tweaks6

--- Frontend/src/components/VehiclePanel.jsx
+++ Frontend/src/components/VehiclePanel.jsx
@@ -13,7 +13,7 @@ const VehiclePanel = (props) => {
                 props.setConfirmRidePanel(true)
                 props.selectVehicle('car')
             }} className='flex border-2 active:border-black  mb-2 rounded-xl w-full p-3  items-center justify-between'>
-                <img className='h-10 w-10 object-contain' src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg" alt="" />
+                <img className='h-10' src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg" alt="" />
                 <div className='ml-2 w-1/2'>
                     <h4 className='font-medium text-base'>ZenoGo<span><i className="ri-user-3-fill"></i>4</span></h4>
                     <h5 className='font-medium text-sm'>2 mins away </h5>
